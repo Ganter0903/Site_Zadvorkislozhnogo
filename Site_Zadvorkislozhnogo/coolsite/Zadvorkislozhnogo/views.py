71cdfb9758bf7d1e3922a94794acee6587a462ab
Ganter <ganter09032003@gmail.com>
2024-06-29 14:46:58 +0300

    create: Главная страница, static files

--- Site_Zadvorkislozhnogo/coolsite/Zadvorkislozhnogo/views.py
+++ Site_Zadvorkislozhnogo/coolsite/Zadvorkislozhnogo/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
 def index(request):
-    return HttpResponse("Страница приложения women.")
+    return render(request, 'Zadvorkislozhnogo/index.html', {'menu': menu, 'title': 'Задворкисложного - официальный сайт'})
+
+def about(request):
+    return render(request, 'Zadvorkislozhnogo/about_site.html', {'title': 'О сайте'})
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
