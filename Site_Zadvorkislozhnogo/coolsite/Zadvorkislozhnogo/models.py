579de15ca558897c47f78f289fb4b20103c078d3
Ganter <ganter09032003@gmail.com>
2024-06-29 14:13:41 +0300

    create: SQL migrations

--- Site_Zadvorkislozhnogo/coolsite/Zadvorkislozhnogo/models.py
+++ Site_Zadvorkislozhnogo/coolsite/Zadvorkislozhnogo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    UserName = models.CharField(max_length=255)
+    UserPhoto = models.ImageField(upload_to="photos/%Y/%m/%d/")
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
